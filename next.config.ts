a84d27f0bada01afa63f9dbaf75b8da50a294395
Saifeleslam Elsayed <saif82820@gmail.com>
2026-06-30 19:00:43 +0300

    next config fix

--- next.config.ts
+++ next.config.ts
@@ -4,15 +4,7 @@ const withNextIntl = createNextIntlPlugin('./src/lib/i18n/request.ts');
 
 const nextConfig = {
   reactStrictMode: true,
-  allowedDevOrigins: ['reed-electrosensitive-jimmy.ngrok-free.dev'],
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost:4000/api/:path*',
-      },
-    ];
-  },
+  allowedDevOrigins: [],
 };
 
 export default withNextIntl(nextConfig);
